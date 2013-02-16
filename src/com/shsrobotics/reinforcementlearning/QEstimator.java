a7f6d8e09915757bb85934f67c83a3e67e1f0c82
robototes2412 <robototes2412@gmail.com>
2013-02-16 12:42:59 -0800

    neural network + test class

---
+++ src/com/shsrobotics/reinforcementlearning/QEstimator.java
@@ -0,0 +1,142 @@
+package com.shsrobotics.reinforcementlearning;
+
+public class QEstimator {
+	
+	private final int hiddenLayers;
+	private final int learningRate;
+	private final int momentum;
+	private final int numberOfOutputs;
+	private final int numberOfInputs;
+	private final int outputLayer;
+	
+	private int[] sizes;
+	private double[][] biases;
+	private double[][][] weights;
+	private double[][] outputs;
+	private double[][] deltas;
+	private double[][][] changes;
+	private double[][] errors;
+	
+	/**
+	 * <h1>Q-Value Estimator</h1>
+	 * @param inputs the number of input parameters
+	 * @param outputs the number of output parameters
+	 * @param hiddenLayers the number of hidden layers
+	 * @param learningRate the learning rate
+	 * @param momentum momentum to prevent local maximization
+	 */
+	public QEstimator(int inputs, int hiddenLayers, int outputs, double learningRate, double momentum) {
+		this.numberOfInputs = inputs;
+		this.numberOfOutputs = outputs;
+		this.hiddenLayers = hiddenLayers;		
+		this.learningRate = hiddenLayers;		
+		this.momentum = hiddenLayers;	
+		this.outputLayer = 1 + this.hiddenLayers;
+		for (int layer = 0; layer <= this.outputLayer; layer++) {
+			if (layer == this.outputLayer) {
+				this.outputs[layer] = zeros(this.numberOfOutputs);
+				this.biases[layer] = rands(this.numberOfOutputs);
+				this.deltas[layer] = zeros(this.numberOfOutputs);
+				this.errors[layer] = zeros(this.numberOfOutputs);
+				this.sizes[layer] = this.numberOfOutputs;
+			} else {				
+				this.outputs[layer] = zeros(this.numberOfInputs);
+				this.biases[layer] = rands(this.numberOfInputs);
+				this.deltas[layer] = zeros(this.numberOfInputs);
+				this.errors[layer] = zeros(this.numberOfInputs);
+				this.sizes[layer] = this.numberOfInputs;				
+			}
+		}
+		for (int layer = 0; layer <= this.outputLayer; layer++) {
+			for (int node = 0; node < this.sizes[layer]; node++) {
+				this.weights[layer][node] = rands(this.sizes[layer + 1]);				
+				this.changes[layer][node] = rands(this.sizes[layer + 1]);				
+			}
+		}		
+	}
+	
+	public void addDataPoint(double[] input, double[]output) {
+		if (input.length != numberOfInputs) {
+			throw new Error("Incorrect number of inputs.");
+		}
+		if (output.length != numberOfOutputs) {
+			throw new Error("Incorrect number of outputs.");
+		}
+		runInput(input);
+		calculateDeltas(output);
+		adjustWeights();
+	}
+
+	private void runInput(double[] input) {
+		outputs[0] = input;
+		for (int layer = 1; layer <= this.outputLayer; layer++) { // for every layer
+			for (int node = 0; node < this.sizes[layer]; node++) { // for every node
+				double weights[] = this.weights[layer][node];
+				double sum = this.biases[layer][node];
+				for (int k = 0; k < weights.length; k++) { // for every connection
+					sum += weights[k] * input[k];
+				}
+				outputs[layer][node] = 1 / (1 + Math.exp(-sum)); // logistic output
+			}
+			input = outputs[layer];
+		}
+	}
+
+	private void calculateDeltas(double[] target) {
+		for (int layer = this.outputLayer; layer >= 0; layer--) { // every layer, backwards
+			for (int node = 0; node < this.sizes[layer]; node++) { // every node
+				double output = this.outputs[layer][node]; // output from node
+
+				double error = 0;
+				if (layer == this.outputLayer) {
+					error = target[node] - output; // final output error
+				}
+				else {
+					double[] currentDeltas = deltas[layer + 1]; // the next layers deltas (we are working backwards)
+					for (int k = 0; k < currentDeltas.length; k++) { // every connection
+						error += currentDeltas[k] * weights[layer + 1][k][node];
+					}
+				}
+				errors[layer][node] = error; // save error
+				deltas[layer][node] = error * output * (1 - output); // save deltas
+			}
+		}
+	}
+
+	private void adjustWeights() {
+		for (int layer = 1; layer <= this.outputLayer; layer++) { // for every layer			
+			double[] incoming = outputs[layer - 1];	
+			for (int node = 0; node < this.sizes[layer]; node++) { // for every node
+				double currentDelta = deltas[layer][node]; // the next layers deltas (we are working backwards)
+				for (int k = 0; k < incoming.length; k++) { // for each incoming connection
+					double currentChange = this.changes[layer][node][k];
+					currentChange = (learningRate * currentDelta * incoming[k]) + (this.momentum * currentChange);
+					
+					changes[layer][node][k] = currentChange;
+					weights[layer][node][k] += currentChange;
+				}
+				biases[layer][node] += learningRate * currentDelta;
+			}
+		}
+	}
+	
+	private double randomWeight() {
+		return Math.random() * 0.4 - 0.2;
+	}
+	
+	private double[] zeros(int size) {
+		double[] toReturn = new double[size];
+		for (int i = 0; i < size; i++) {
+			toReturn[i] = 0;
+		}
+		return toReturn;
+	}
+	
+	private double[] rands(int size) {
+		double[] toReturn = new double[size];
+		for (int i = 0; i < size; i++) {
+			toReturn[i] = randomWeight();
+		}
+		return toReturn;
+	}
+}
