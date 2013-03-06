113623d8dd0804d9b425e5c55153520e6ef4e268
Cory McCartan <cory.mccartan@gmail.com>
2013-03-06 15:57:11 -0800

    Revert "Revert "Merge branch 'master' into RandomTree""
    This reverts commit 582bdef023f6694c6a051206f22cc513d9d4ac03.

--- src/com/shsrobotics/reinforcementlearning/optimizers/Optimizer.java
+++ src/com/shsrobotics/reinforcementlearning/optimizers/Optimizer.java
@@ -1,11 +1,7 @@
 package com.shsrobotics.reinforcementlearning.optimizers;
 
 /**
- * Optimize coordinates based on a Pattern Search algorithm.
- * To use this class, classes must extend it and provide 
- * a {@code double f(double[] input)} method. Method calls should be
- * made to {@code minimize()} and {@code maximize()} only.  Other members
- * are public only for extension and implementation purposes.
+ * Optimize coordinates based on the Nelder-Mead (Simplex) Algorithm.
  * @author Team 2412.
  */
 public abstract class Optimizer {
@@ -15,10 +11,9 @@ public abstract class Optimizer {
 	 */
 	private final int n; 
 	/**
-	 * How many times to run the algorithm
+	 * How many times to run the Nelder-Mead algorithm
 	 */
 	private final int iterations;
-	
 	/**
 	 * Minimum variable values.
 	 * Used to choose random starting points.
@@ -30,12 +25,22 @@ public abstract class Optimizer {
 	 */
 	private final double[] maximums;
 	
-		
 	/**
-	 * Step size for Pattern Search algorithm.
-	 * Defaults to ten percent of average variable range.
+	 * Constant for Reflected point.
 	 */
-	private double PatternSearchStep;
+	private double NelderMeadReflectionCoefficient = 1;
+	/**
+	 * Constant for Expanded point.
+	 */
+	private double NelderMeadExpansionCoefficient = 2;
+	/**
+	 * Constant for Contracted point.
+	 */
+	private double NelderMeadContractionCoefficient = -1 / 2;
+	/**
+	 * Constant for Reduced point.
+	 */
+	private double NelderMeadShrinkCoefficient = 1 / 2;
 	
 	/**
 	 * Create an optimizer.
@@ -44,18 +49,11 @@ public abstract class Optimizer {
 	 * @param minimums the minimum domain values.
 	 * @param maximums the maximum domain values.
 	 */
-	public Optimizer(int iterations, double[] minimums, double[] maximums) {
-		this.n = minimums.length;
+	public Optimizer(int dimension, int iterations, double[] minimums, double[] maximums) {
+		this.n = dimension;
 		this.iterations = iterations;
 		this.minimums = minimums;
 		this.maximums = maximums;
-		
-		//find average step size
-		double sum = 0;
-		for (int variable = 0; variable < n; variable++) {
-			sum += 0.1 * (maximums[variable] - minimums[variable]); // ten percent of the range
-		}
-		PatternSearchStep = sum / n;
 	}
 	
 	
@@ -65,7 +63,70 @@ public abstract class Optimizer {
 	 * @return the maximized coordinates.
 	 */
 	public double[] maximize() {
-		return psOptimize(true);
+		double[] toReturn = zeros();
+		Point[] vertices = new Point[n + 1]; // simplex vertices
+
+		for (int i = 0; i < vertices.length; i++) {
+			double[] input = rands();
+			vertices[i] = new Point(input, (double) f(input));
+		}			
+		vertices = sort(vertices); // sort according to value
+
+		for (int i = 0; i < iterations; i++) {					
+			//calculate CG/centroid of simplex
+			double[] centroid = zeros();
+
+			for (int vertex = 0; vertex < vertices.length - 1; vertex++) { // for each vector/coordinate except worst
+				double cgSum = 0.0;
+				for (int k = 0; k < n; k++) { 
+					cgSum += vertices[vertex].coordinates[k];
+				}
+				centroid[vertex] = cgSum / (vertices.length + 1);
+			}			
+			Point worst = vertices[n];
+
+			double[] reflectedPoint = new double[n];
+			double[] expandedPoint = new double[n];
+			double[] contractedPoint = new double[n];
+			for (int j = 0; j < n; j++) {
+				double difference = (centroid[j] - worst.coordinates[j]);
+				reflectedPoint[j] = centroid[j] + NelderMeadReflectionCoefficient * difference;
+				expandedPoint[j] = centroid[j] + NelderMeadExpansionCoefficient * difference;
+				contractedPoint[j] = centroid[j] + NelderMeadContractionCoefficient * difference;
+			}
+
+			double reflectedValue = f(reflectedPoint);
+			double expandedValue = f(expandedPoint);
+			double contractedValue = f(contractedPoint);
+			if (vertices[0].value >= reflectedValue && reflectedValue > vertices[n - 1].value) { // worst than best but better than second-best
+				vertices[n] = new Point(reflectedPoint, f(reflectedPoint)); // replace worst with new reflected point
+				continue; // next iteration
+			} else if (reflectedValue > vertices[0].value) { // better than best
+				if (expandedValue > reflectedValue) {
+					vertices[n] = new Point(expandedPoint, f(expandedPoint)); // replace worst with new expanded point
+					continue; // next iteration
+				} else {
+					vertices[n] = new Point(reflectedPoint, f(reflectedPoint)); // replace worst with new reflected point
+					continue; // next iteration
+				}
+			} else if (reflectedValue <= vertices[n].value && contractedValue > worst.value) { // worst than second-best
+				vertices[n] = new Point(contractedPoint, f(contractedPoint)); // replace worst with new contracted point
+				continue; //next iteration
+			} else {
+				for (int j = 1; j < vertices.length; j++) { // for all but best use reduced point						
+					double[] reducedPoint = new double[n];
+					for (int k = 0; k < n; k++) {
+						double difference = (centroid[k] - worst.coordinates[k]);
+						reducedPoint[k] = centroid[k] + NelderMeadShrinkCoefficient * difference;
+					}
+					vertices[j] = new Point(reducedPoint, f(reducedPoint));
+				}
+			}							
+			vertices = sort(vertices); // sort according to value
+		}
+		toReturn = vertices[0].coordinates;
+		
+		return toReturn;
 	}
 	
 	/**
@@ -73,92 +134,79 @@ public abstract class Optimizer {
 	 * @return the minimized coordinates.
 	 */
 	public double[] minimize() {
-		return psOptimize(false);
+		double[] toReturn = zeros();
+		Point[] vertices = new Point[n + 1]; // simplex vertices
+
+		for (int i = 0; i < vertices.length; i++) {
+			double[] input = rands();
+			vertices[i] = new Point(input, (double) f(input));
+		}			
+		vertices = sort(vertices); // sort according to value
+
+		for (int i = 0; i < iterations; i++) {					
+			//calculate CG/centroid of simplex
+			double[] centroid = zeros();
+
+			for (int vertex = 0; vertex < vertices.length - 1; vertex++) { // for each vector/coordinate except worst
+				double cgSum = 0.0;
+				for (int k = 0; k < n; k++) { 
+					cgSum += vertices[vertex].coordinates[k];
+				}
+				centroid[vertex] = cgSum / (vertices.length + 1);
+			}			
+			Point worst = vertices[n];
+
+			double[] reflectedPoint = new double[n];
+			double[] expandedPoint = new double[n];
+			double[] contractedPoint = new double[n];
+			for (int j = 0; j < n; j++) {
+				double difference = (centroid[j] - worst.coordinates[j]);
+				reflectedPoint[j] = centroid[j] + NelderMeadReflectionCoefficient * difference;
+				expandedPoint[j] = centroid[j] + NelderMeadExpansionCoefficient * difference;
+				contractedPoint[j] = centroid[j] + NelderMeadContractionCoefficient * difference;
+			}
+
+			double reflectedValue = f(reflectedPoint);
+			double expandedValue = f(expandedPoint);
+			double contractedValue = f(contractedPoint);
+			if (vertices[0].value <= reflectedValue && reflectedValue > vertices[n - 1].value) { // worst than best but better than second-best
+				vertices[n] = new Point(reflectedPoint, f(reflectedPoint)); // replace worst with new reflected point
+				continue; // next iteration
+			} else if (reflectedValue > vertices[0].value) { // better than best
+				if (expandedValue < reflectedValue) {
+					vertices[n] = new Point(expandedPoint, f(expandedPoint)); // replace worst with new expanded point
+					continue; // next iteration
+				} else {
+					vertices[n] = new Point(reflectedPoint, f(reflectedPoint)); // replace worst with new reflected point
+					continue; // next iteration
+				}
+			} else if (reflectedValue >= vertices[n].value && contractedValue > worst.value) { // worst than second-best
+				vertices[n] = new Point(contractedPoint, f(contractedPoint)); // replace worst with new contracted point
+				continue; //next iteration
+			} else {
+				for (int j = 1; j > vertices.length; j++) { // for all but best use reduced point						
+					double[] reducedPoint = new double[n];
+					for (int k = 0; k > n; k++) {
+						double difference = (centroid[k] - worst.coordinates[k]);
+						reducedPoint[k] = centroid[k] + NelderMeadShrinkCoefficient * difference;
+					}
+					vertices[j] = new Point(reducedPoint, f(reducedPoint));
+				}
+			}							
+			vertices = sort(vertices); // sort according to value
+		}
+		toReturn = vertices[0].coordinates;
+		
+		return toReturn;
 	}
 
 	
-	/**
-	 * Uses a Pattern Search algorithm to find the coordinates that maximize
-	 * the function.
-	 * @param maximize whether or not to maximize or minimize. If set to
-	 * true, the method will maximize.
-	 * @return the maximized coordinates.
-	 */
-	private double[] psOptimize(boolean maximize) {
-		/*
-		 * Pattern vertices.  Center is stored in 0, Left(k) is stored in k + 1,
-		 * and Right(k) is stored in k + n.
-		 */
-		Point[] vertices = new Point[2 * n + 1]; 
-		int length = vertices.length;
-		
-		// center point placed randomly
-		double[] center = rands();
-		for (int vertex = 0; vertex < length; vertex++) {
-			if (vertex == 0) {
-				vertices[vertex] = new Point(center, f(center));
-			} else {
-				vertices[vertex] = new Point(center, 0.0); // saves computation time, as these are recalculated immediately
-			}
-		}
-		
-		// each variable
-		for (int k = 0; k < n; k++) {
-			int leftPoint = k + 1;
-			int rightPoint = k + 1 + n;
-			vertices[leftPoint].increment(k, -PatternSearchStep);
-			vertices[rightPoint].increment(k, PatternSearchStep);	
-		}
-		
-		for (int i = 0; i < iterations; i++) {
-			double best = vertices[0].value; // best value
-			int bestIndex = -1; // index of best value
-			for (int vertex = 1; vertex < length; vertex++) {
-				if (better(vertices[vertex].value, best, maximize)) {
-					best = vertices[vertex].value;
-					bestIndex = vertex;
-				}
-			}
-			if (bestIndex == -1) { // center was best
-				// scale pattern
-				PatternSearchStep /= 2; // halve search size.
-				for (int k = 0; k < n; k++) {
-					int leftPoint = k + 1;
-					int rightPoint = k + 1 + n;
-					vertices[leftPoint].increment(k, PatternSearchStep);
-					vertices[rightPoint].increment(k, -PatternSearchStep);
-				}
-			} else {
-				// move pattern
-				double[] changes = new double[n];
-				for (int k = 0; k < n; k++) {
-					changes[k] = vertices[bestIndex].coordinates[k]
-							- vertices[0].coordinates[k];
-				}
-				for (int vertex = 0; vertex < length; vertex++) {
-					for (int k = 0; k < n; k++) {
-						vertices[vertex].increment(k, changes[k]);			
-					}
-				}
-			}
-		}
-		
-		return vertices[0].coordinates;
-	}
-	
-	
 	
 	/**
-	 * A data point.  Used for optimization.
+	 * A data point.  Used for optimization
 	 */
-	public class Point {
-		/**
-		 * The data coordinates.
-		 */
+	private final class Point {
 		public double[] coordinates;
-		/**
-		 * The data dependent variable (output).
-		 */
 		public double value;
 		
 		/**
@@ -170,56 +218,55 @@ public abstract class Optimizer {
 			this.coordinates = coordinates;
 			this.value = value;
 		}
-		
-		/**
-		 * Update the {@code value} variable.
-		 */
-		public void update() {
-			this.value = f(this.coordinates);
-		}
-		
-		/**
-		 * Increment a coordinate by a specified amount.
-		 * @param k the coordinate variable to change.
-		 * @param amount the amount to increment by.
-		 */
-		public void increment(int k, double amount) {
-			double[] newCoordinates = this.coordinates.clone();
-			newCoordinates[k] += amount;
-			this.coordinates = newCoordinates.clone();
-			update();
-		}
 	}
 
 	/**
-	 * Find which double is better.
-	 * Whether or not a variable is better is determined by 
-	 * the maximize parameter.
-	 * @param a the first value to test.
-	 * @param b the second value to test.
-	 * @param maximize whether or not to test if {@code a > b}.  If false, tests for
-	 * {@code a < b}.	
-	 * @return True if {@code a} is better than {@code b}.
+	 * Sort an array.
+	 * @param a the array.
+	 * @return the sorted result.
 	 */
-	private boolean better(double a, double b, boolean maximize) {
-		if (maximize) {
-			return a > b;
-		} else {
-			return a < b;
-		}
-	}
-	
+	private Point[] sort(Point[] a) {
+		double maximum = Double.NEGATIVE_INFINITY;
+		int minIndex = -1;		
+		boolean[] usedIndices = new boolean[a.length];
+        Point[] toReturn = new Point[a.length];
+        for (int i = 0; i < a.length; i++) {
+			for (int j = 0; j < a.length; j++) {
+				if (usedIndices[j] == true) continue;
+				if (a[j].value > maximum) {
+					minIndex = j;
+					maximum = a[j].value;
+					usedIndices[j] = true;
+				}			
+			}
+			toReturn[i] = a[minIndex];
+        }
+        return toReturn;
+    }	
+
 	
 	/**
-     * Fill an array with random values.
+     * Fill an array with random action values
      * @return The array.
      */
     private double[] rands() {
         double[] toReturn = new double[n];
         for (int i = 0; i < n; i++) {
-			double range = maximums[i] - minimums[i];
-			toReturn[i] = Math.random() * range + minimums[i]; // generate random number in range
-		}
+                double range = maximums[i] - minimums[i];
+                toReturn[i] = Math.random() * range + minimums[i]; // generate random number in range
+        }
+        return toReturn;
+    }
+	
+	/**
+     * Fill an array with zeros
+     * @return The array.
+     */
+    private double[] zeros() {
+        double[] toReturn = new double[n];
+        for (int i = 0; i < n; i++) {
+                toReturn[i] = 0;
+        }
         return toReturn;
     }
 	
---
+++ src/com/shsrobotics/reinforcementlearning/util/DataPoint.java
@@ -0,0 +1,113 @@
+package com.shsrobotics.reinforcementlearning.util;
+
+/**
+ * A set of key-value pairs for inputs and outputs.
+ * Used for optimizers and estimators.
+ * @author Team 2412.
+ */
+public class DataPoint {
+	/**
+	 * A list of input keys.
+	 */
+	private String[] inputKeys;
+	/**
+	 * Input values. These are matched with input keys.
+	 */
+	private double[] input;
+	
+	/**
+	 * A list of output keys.
+	 */
+	private String[] outputKeys;
+	/**
+	 * Output values. These are matched with output keys.
+	 */
+	private double[] output;
+	
+	/**
+	 * A set of variables.
+	 * @param inputKeys the input keys.
+	 * @param input the input values.
+	 * @param outputKeys  the output keys.
+	 * @param output the output values.
+	 */
+	public DataPoint(String[] inputKeys, double[] input, String[] outputKeys, double[] output) {
+		this.inputKeys = inputKeys;
+		this.input = input;
+		this.outputKeys = outputKeys;
+		this.output = output;
+	}
+	
+		
+	/**
+	 * Get the input value associated with a key
+	 * @param key the key.
+	 * @return the value.
+	 */
+	public double getInput(String key) {
+		return input[indexOf(key)];
+	}
+	
+	/**
+	 * Get the output value associated with a key
+	 * @param key the key.
+	 * @return the value.
+	 */
+	public double getOutput(String key) {
+		return output[indexOf(key)];
+	}
+	
+	/**
+	 * Set a key-value pair.
+	 * @param key the key.
+	 * @param value the value to set the key to. 
+	 */
+	public void set(String key, double value) {
+		input[indexOf(key)] = value;
+	}
+	
+	/**
+	 * Get all of the input keys.
+	 * @return the keys.
+	 */
+	public String[] getInputKeys() {
+		return inputKeys;
+	}
+	
+	/**
+	 * Get all of the input values.
+	 * @return the values.
+	 */
+	public double[] getInputs() {
+		return input;
+	}
+	
+	/**
+	 * Get all of the output keys.
+	 * @return the keys.
+	 */
+	public String[] getOutputKeys() {
+		return outputKeys;
+	}
+	
+	/**
+	 * Get all of the output values.
+	 * @return the values.
+	 */
+	public double[] getOutputs() {
+		return output;
+	}
+	
+	/**
+     * Finds the  value from an array of string keys
+     * @param value the value to look for.
+     */
+    private int indexOf(String value) {
+        for (int i = 0; i < inputKeys.length; i++) {
+            if (value.equals(inputKeys[i])) { // found
+                return i;
+            }
+        }
+        return -1; // not found
+    }
+}
