8837a9647fe7adbe86d7761761ad8969bdf753ba
Max Orth <max.orth96@gmail.com>
2013-03-06 19:39:28 -0800

    added parametered matrix stuff

---
+++ src/com/shsrobotics/reinforcementlearning/math/MatrixParameter.java
@@ -0,0 +1,15 @@
+package com.shsrobotics.reinforcementlearning.math;
+
+/**
+ *	
+ * @author Max
+ */
+public interface MatrixParameter {
+	
+	public double getComputed();
+	
+	public double getParameter();
+	
+	public void setParameter(double param);
+	
+}
---
+++ src/com/shsrobotics/reinforcementlearning/math/ParameteredMatrix.java
@@ -0,0 +1,49 @@
+package com.shsrobotics.reinforcementlearning.math;
+
+/**
+ *
+ * @author Max
+ */
+public class ParameteredMatrix implements Matrix {
+
+	@Override
+	public int getRowSize() {
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	@Override
+	public int getColumSize() {
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	@Override
+	public double getValue(int row, int colum) {
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	@Override
+	public void setValue(int row, int colum, double value) {
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	@Override
+	public void scale(double scale) {
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	@Override
+	public Matrix add(Matrix m) {
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	@Override
+	public Matrix subtract(Matrix m) {
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	@Override
+	public Matrix multiply(Matrix right) {
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+	
+}
