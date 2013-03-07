2532f51259ccc50df5bb3a4aa681637c892fb63c
Cory McCartan <cory.mccartan@gmail.com>
2013-03-06 17:01:58 -0800

    fixes #17
    the fixes #16 commit was incorrectly named, the bug was #17

--- src/com/shsrobotics/reinforcementlearning/util/RandomDecisionTree.java
+++ src/com/shsrobotics/reinforcementlearning/util/RandomDecisionTree.java
@@ -144,7 +144,7 @@ public class RandomDecisionTree {
 	 */
 	private double average(DataPoint[] data) {
 		double sum = 0.0;
-		double length = data.length;
+		double length = data.length; // for faster loop
 		for (int i = 0; i < length; i++) {
 			sum += data[i].getOutputs()[0];
 		}
