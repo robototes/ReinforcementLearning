417c0b25c6ebf33e374621995b9b866282342a92
Max Orth <max.orth96@gmail.com>
2013-03-06 17:58:30 -0800

    added MatrixSizeError and operations
    operations added were addition, subtraction, and scale

--- src/com/shsrobotics/reinforcementlearning/math/Matrix.java
+++ src/com/shsrobotics/reinforcementlearning/math/Matrix.java
@@ -3,8 +3,7 @@ package com.shsrobotics.reinforcementlearning.math;
 /** 
  *	Represents an m<i>x</i>n dimensional matrix and supports these basic operations:
  *	<ul>
- *		<li>scalar multiply</li>	
- *		<li>scalar divide</li>
+ *		<li>scaling</li>
  *		<li>matrix addition</li>
  *		<li>matrix subtraction</li>
  *		<li>matrix multiplication</li>
@@ -16,18 +15,85 @@ package com.shsrobotics.reinforcementlearning.math;
  */
 public class Matrix {
 	
-	double[] body;
+	double[][] body;
 	
 	private Matrix() {
 		;
 	}
 	
 	public Matrix(int rows, int colums) {
-		body = new double[rows*colums];
+		body = new double[rows][colums];
 	}
 	
 	public Matrix(int size) {
-		body = new double[size*size];
+		body = new double[size][size];
+	}
+	
+	/**
+	 * Scales <code>this</code> by multiplying each value in the Matrix by <code>scale</code>.
+	 * 
+	 * @param scale The scalar to scale by.
+	 */
+	
+	public void scale(double scale) {
+		for ( int r = 0; r < body.length; r++ ) {
+			for ( int c = 0; c < body[0].length; c++ ) {
+				body[r][c] *= scale;
+			}
+		}
+	}
+	
+	/**
+	 * Adds <code>this</code> and <code>m</code>.
+	 * 
+	 * @param m The Matrix to add.
+	 * @return The resulting Matrix.
+	 */
+	
+	public Matrix add(Matrix m) {
+		
+		Matrix result = new Matrix(this.body.length,this.body[0].length);
+		
+		if ( this.body.length != m.body.length) {
+			throw new MatrixSizeError("Incompatible row sizes:", m.body.length, this.body.length);
+		}
+		else if ( this.body[0].length != m.body[0].length ) {
+			throw new MatrixSizeError("Incompatible colum sizes:", m.body.length, this.body.length);
+		}
+		
+		for ( int r = 0; r < body.length; r++ ) {
+			for ( int c = 0; c < body[0].length; c++ ) {
+				result.body[r][c] = this.body[r][c] + m.body[r][c];
+			}
+		}
+		
+		return result;
+	}
+	
+	/**
+	 * Subtracts <code>m</code> from <code>this</code>.
+	 * @param m The Matrix to subtract.
+	 * @return The resulting Matrix.
+	 */
+	
+	public Matrix subtract(Matrix m) {
+		
+		if ( this.body.length != m.body.length) {
+			throw new MatrixSizeError("Incompatible row sizes:", m.body.length, this.body.length);
+		}
+		else if ( this.body[0].length != m.body[0].length ) {
+			throw new MatrixSizeError("Incompatible colum sizes:", m.body.length, this.body.length);
+		}
+		
+		Matrix result = new Matrix(this.body.length,this.body[0].length);
+		
+		for ( int r = 0; r < body.length; r++ ) {
+			for ( int c = 0; c < body[0].length; c++ ) {
+				result.body[r][c] = this.body[r][c] - m.body[r][c];
+			}
+		}
+		
+		return result;
 	}
 	
 }
---
+++ src/com/shsrobotics/reinforcementlearning/math/MatrixSizeError.java
@@ -0,0 +1,25 @@
+package com.shsrobotics.reinforcementlearning.math;
+
+/**	
+ *	Thrown when Matrix sizes are not compatible for a given operation.
+ *	@author Max
+ */
+public class MatrixSizeError extends Error {
+	
+	/**
+	 * Call this when there is at least one dimension that is incompatible during
+	 * a Matrix operation.
+	 * 
+	 * Note that the found parameter is what was found in the second Matrix in the 
+	 * parameter of the Matrix operation, NOT the <code>this</code> object.
+	 * 
+	 * @param msg A statement that says what was incompatible.
+	 * @param found The size of the dimension that was incompatible.
+	 * @param required The required size of the other matrix.
+	 */
+	
+	
+	public MatrixSizeError(String msg, int found, int required) {
+		super(msg + " Expected " + required + ", got " + found);
+	}
+}
