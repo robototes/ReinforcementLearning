61ed20020bdb85bcfbef6f3e54fa64c8641c2a2a
Cory McCartan <cory.mccartan@gmail.com>
2013-03-06 16:55:18 -0800

    optimizations

--- src/com/shsrobotics/reinforcementlearning/util/RandomDecisionTree.java
+++ src/com/shsrobotics/reinforcementlearning/util/RandomDecisionTree.java
@@ -140,10 +140,11 @@ public class RandomDecisionTree {
 	 */
 	private double average(DataPoint[] data) {
 		double sum = 0.0;
-		for (int i = 0; i < data.length; i++) {
+		double length = data.length;
+		for (int i = 0; i < length; i++) {
 			sum += data[i].getOutputs()[0];
 		}
-		return sum / data.length;
+		return sum / length;
 	}
 
 	/**
