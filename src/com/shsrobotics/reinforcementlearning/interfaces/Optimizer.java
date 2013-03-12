0171f356b118a47f11024d76a90d794eddc6bce0
Cory McCartan <cory.mccartan@gmail.com>
2013-03-12 16:20:39 -0700

    finished optimizer interface

--- src/com/shsrobotics/reinforcementlearning/interfaces/Optimizer.java
+++ src/com/shsrobotics/reinforcementlearning/interfaces/Optimizer.java
@@ -1,5 +1,24 @@
 package com.shsrobotics.reinforcementlearning.interfaces;
 
-public interface Optimizer {
+import com.shsrobotics.reinforcementlearning.util.DataPoint;
+
+public abstract class Optimizer {	
+	/**
+	 * Minimize the function.
+	 * @return the minimized coordinates
+	 */
+	public abstract DataPoint[] minimize();
 	
+	/**
+	 * Minimize the function.
+	 * @return the maximized coordinates.
+	 */
+	public abstract DataPoint[] maximize();
+	
+	/**
+	 * The function to optimize.
+	 * @param input the domain (input).
+	 * @return the range (output).
+	 */
+	public abstract double f(double[] input);
 }
