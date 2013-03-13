7521f5c631124d01ec136b0ecaeaa9ee49850135
Cory McCartan <cory.mccartan@gmail.com>
2013-03-13 16:48:01 -0700

    Updated Supervised Learner abstract class.

--- src/com/shsrobotics/reinforcementlearning/supervisedlearners/SupervisedLearner.java
+++ src/com/shsrobotics/reinforcementlearning/supervisedlearners/SupervisedLearner.java
@@ -1,8 +1,47 @@
 package com.shsrobotics.reinforcementlearning.supervisedlearners;
 
+import com.shsrobotics.reinforcementlearning.util.DataPoint;
+
 /**
  * A supervised learner.
  */
-public interface SupervisedLearner {
+public abstract class SupervisedLearner {
+	/**
+	 * Minimum values for each input variable.
+	 */
+	protected double[] minimums;
+	/**
+	 * Maximum values for each input variable.
+	 */
+	protected double[] maximums;
 	
+	/**
+	 * The model data.
+	 */
+	protected DataPoint[] data;
+	
+	
+	/**
+	 * Create a supervised learner.
+	 * @param minimums see {@link #minimums}
+	 * @param maximums see {@link #maximums}
+	 */
+	public SupervisedLearner(double[] minimums, double[] maximums) {
+		this.minimums = minimums;
+		this.maximums = maximums;
+	}
+	
+	/**
+	 * Update the model with new experiences.
+	 * @param input the input values.
+	 * @param output the correct output value.
+	 */
+	public abstract void update(double[] input, double output);
+	
+	/**
+	 * Query the learner for a prediction.
+	 * @param input the input values.
+	 * @return the predicted output value.
+	 */
+	public abstract double query(double[] input);
 }
