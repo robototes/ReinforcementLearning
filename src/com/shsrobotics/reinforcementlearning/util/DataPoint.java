2db21ff746a3e2a0ba85d991ecfc2372e8c6563d
Cory McCartan <cory.mccartan@gmail.com>
2013-06-26 17:21:16 -0700

    updated data point class

--- src/com/shsrobotics/reinforcementlearning/util/DataPoint.java
+++ src/com/shsrobotics/reinforcementlearning/util/DataPoint.java
@@ -6,7 +6,7 @@ package com.shsrobotics.reinforcementlearning.util;
  * <p/>
  * @author Team 2412.
  */
-public class DataPoint {
+public class DataPoint implements Cloneable{
 
 	/**
 	 * A list of input keys.
@@ -42,7 +42,48 @@ public class DataPoint {
 		this.outputKeys = outputKeys;
 		this.output = output;
 	}
-
+	
+	/**
+	 * A set of variables.
+	 * <p/>
+	 * @param input the input values.
+	 * @param output the output value.
+	 */
+	public DataPoint(double[] input, double output) {
+		String[] inputKeys = new String[input.length];
+		String[] outputKeys = {"Output"};
+		double[] outputArray = {output};
+		for (int i = 0; i < input.length; i++) {
+			inputKeys[i] = String.valueOf(i);
+		}
+		this.inputKeys = inputKeys;
+		this.input = input;
+		this.outputKeys = outputKeys;
+		this.output = outputArray;
+	}
+	
+	/**
+	 * A set of variables.
+	 * <p/>
+	 * @param keys the input keys.
+	 * @param values  the input values.
+	 * @param isInput true if is an input point, false if is an output point.
+	 */
+	public DataPoint(String[] keys, double[] values, boolean isInput) {
+		if (isInput) {
+			this.inputKeys = keys;
+			this.input = values;
+			this.outputKeys = null;
+			this.output = null;
+		} else {
+			this.inputKeys = null;
+			this.input = null;
+			this.outputKeys = keys;
+			this.output = values;
+		}
+	}
+	
+	
 	/**
 	 * Get the input value associated with a key
 	 * <p/>
@@ -69,8 +110,34 @@ public class DataPoint {
 	 * @param key the key.
 	 * @param value the value to set the key to.
 	 */
-	public void set(String key, double value) {
-		input[indexOf(key)] = value;
+	public void setInput(String key, double value) {
+		double[] newInput = input.clone();
+		newInput[indexOf(key)] = value;
+		input = newInput.clone();
+	}
+	
+	/**
+	 * Set a key-value pair.
+	 * <p/>
+	 * @param index the index of a key.
+	 * @param value the value to set the key to.
+	 */
+	public void setOutput(int index, double value) {
+		double[] newOutput = input.clone();
+		newOutput[index] = value;
+		output = newOutput.clone();
+	}
+	
+	/**
+	 * Set a key-value pair.
+	 * <p/>
+	 * @param index the index of a key.
+	 * @param value the value to set the key to.
+	 */
+	public void setInput(int index, double value) {
+		double[] newInput = input.clone();
+		newInput[index] = value;
+		input = newInput.clone();
 	}
 
 	/**
@@ -108,18 +175,45 @@ public class DataPoint {
 	public double[] getOutputs() {
 		return output;
 	}
-
+	
+	/**
+	 * Get all the output value.
+	 * <p/>
+	 * @return the values.
+	 */
+	public double getOutput() {
+		return output[0];
+	}
+	
 	/**
 	 * Finds the value from an array of string keys
 	 * <p/>
 	 * @param value the value to look for.
 	 */
 	private int indexOf(String value) {
-		for (int i = 0; i < inputKeys.length; i++) {
-			if (value.equals(inputKeys[i])) { // found
-				return i;
+		if (inputKeys != null) {
+			for (int i = 0; i < inputKeys.length; i++) {
+				if (value.equals(inputKeys[i])) { // found
+					return i;
+				}
 			}
+			return -1; // not found
+		} else {
+			for (int i = 0; i < outputKeys.length; i++) {
+				if (value.equals(outputKeys[i])) { // found
+					return i;
+				}
+			}
+			return -1; // not found
 		}
-		return -1; // not found
+	}
+	
+	@Override
+	public DataPoint clone() {
+		try {
+			return (DataPoint) super.clone();
+		} catch (CloneNotSupportedException ex) {
+			return null;
+		}
 	}
 }
